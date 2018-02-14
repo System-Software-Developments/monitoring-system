3d26a51b40afa072d2fdbd9a19ac7640c6b7fec3
Richard Li <richard@datawire.io>
2018-02-14 09:11:34 -0500

    fixes to deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 kubectl create ns monitoring-system
-COMPONENT_DIRS=( "./grafana" "./prometheus-operator" "./prometheus" )
+COMPONENT_DIRS=( "./grafana" "./prometheus-operator" "./prom-argonath" "./argonath")
 for i in "${COMPONENT_DIRS[@]}" ; do
    if [ -d "$i" ]; then
      pushd "$i"
